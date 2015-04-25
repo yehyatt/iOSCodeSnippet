34c966de17ae484165e0f129304ad97c381aa0ba
yahyatt <yehyatt@gmail.com>
2015-04-25 07:18:35 +0300

    Initial Commit

---
+++ flipappdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flipappdemo
+//
+//  Created by Yahya Titi on 4/25/15.
+//  Copyright (c) 2015 Yahya Titi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
